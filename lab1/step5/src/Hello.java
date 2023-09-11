d9c9a597f340b222226a0aad462a1571904f2560
Kango911 <lipenkov.a61@gmail.com>
2023-09-11 09:52:41 +0500

    step5 finish

--- lab1/step5/src/Hello.java
+++ lab1/step5/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         Dog mydog = new Dog();
         System.out.println("Dog: "+ mydog.voice());
         System.out.println("2 + 2 = " + ExternalSummator.sum(2,2));
-        System.out.println("Finish Step 4");
+        System.out.println("Secret cod: " + SecretSafe.SECRET_CODE);
+        System.out.println("Finish Step 5");
     }
 }
